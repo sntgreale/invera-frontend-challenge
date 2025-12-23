62ab64e6f60a95de2ba9997b32a32df4d3c15e91
sntgreale <sntg.reale@gmail.com>
2025-12-23 16:10:54 -0300

    Initial commit. Main structure and basic configuration.

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div className="w-full h-full flex-col">Main Page</div>;
+};
+
+export default Dashboard;
---
+++ client/vite.config.ts
@@ -0,0 +1,23 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
